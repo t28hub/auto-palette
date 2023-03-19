3c2a2ee5a69b53d1f8158a7c3137507607ccce41
t28hub <t28oogle@gmail.com>
2023-03-19 14:29:22 +0900

    Add an util funcion is_close_to and rewrite assert_close_to

--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -1,15 +1,20 @@
+use num_traits::Float;
+
+/// Check whether the difference between lhs and the rhs is less than the max difference.
+#[inline(always)]
+pub fn is_close_to<F: Float>(lhs: F, rhs: F, max_diff: F) -> bool {
+    let diff = (lhs - rhs).abs();
+    diff < max_diff
+}
+
 #[macro_export]
 macro_rules! assert_close_to(
     ($lhs: expr, $rhs: expr) => ({
        assert_close_to!($lhs, $rhs, 0.01)
     });
     ($lhs: expr, $rhs: expr, $max_diff: expr) => ({
-        let diff = ($lhs - $rhs).abs();
-        if diff > $max_diff {
-           panic!(
-               "lhs(`{:?}`) is not similar to rhs(`{:?}`) (diff: `{:?}`)",
-               $lhs, $rhs, diff
-           )
+        if !$crate::macros::is_close_to($lhs, $rhs, $max_diff) {
+           panic!("lhs(`{:?}`) is not similar to rhs(`{:?}`)", $lhs, $rhs)
         }
     });
 );
