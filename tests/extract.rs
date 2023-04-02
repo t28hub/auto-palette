5b9a55727c928c6ba186ee382e8290749efd8dec
t28hub <t28oogle@gmail.com>
2023-04-02 16:29:39 +0900

    Add integration tests using DBSCAN and HDBSCAN

--- tests/extract.rs
+++ tests/extract.rs
@@ -3,14 +3,34 @@ extern crate image;
 use auto_palette::{Algorithm, Palette, SimpleImageData};
 
 #[test]
-fn extract() {
+fn extract_with_gmeans() {
     let img = image::open("./tests/images/img.png").unwrap();
-    let width = img.width();
-    let height = img.height();
-    let image_data = SimpleImageData::new(img.into_bytes(), width, height).unwrap();
+    let image_data =
+        SimpleImageData::new(img.as_bytes().to_vec(), img.width(), img.height()).unwrap();
+
     let palette: Palette<f64> = Palette::extract(&image_data, Algorithm::GMEANS);
-    palette
-        .get_swatches(6)
-        .iter()
-        .for_each(|swatch| println!("{:?}", swatch));
+    let swatches = palette.get_swatches(5);
+    assert_eq!(swatches.len(), 5);
+}
+
+#[test]
+fn extract_with_dbscan() {
+    let img = image::open("./tests/images/img.png").unwrap();
+    let image_data =
+        SimpleImageData::new(img.as_bytes().to_vec(), img.width(), img.height()).unwrap();
+
+    let palette: Palette<f64> = Palette::extract(&image_data, Algorithm::DBSCAN);
+    let swatches = palette.get_swatches(5);
+    assert_eq!(swatches.len(), 5);
+}
+
+#[test]
+fn extract_with_hdbscan() {
+    let img = image::open("./tests/images/img.png").unwrap();
+    let image_data =
+        SimpleImageData::new(img.as_bytes().to_vec(), img.width(), img.height()).unwrap();
+
+    let palette: Palette<f64> = Palette::extract(&image_data, Algorithm::HDBSCAN);
+    let swatches = palette.get_swatches(5);
+    assert_eq!(swatches.len(), 5);
 }
