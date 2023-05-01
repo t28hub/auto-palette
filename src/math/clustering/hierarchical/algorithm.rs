48ba2a3b1557690c7af60676ddb8f07c968ed8dc
t28hub <t28oogle@gmail.com>
2023-05-01 16:30:17 +0900

    Removed unused functions fit_with_linkage, partition and bfs in HierarchicalClustering

--- src/math/clustering/hierarchical/algorithm.rs
+++ src/math/clustering/hierarchical/algorithm.rs
@@ -1,4 +1,3 @@
-use crate::math::clustering::hierarchical::linkage::Linkage;
 use crate::math::clustering::hierarchical::node::HierarchicalNode;
 use crate::math::clustering::hierarchical::union_find::UnionFind;
 use crate::math::graph::edge::Edge;
@@ -6,10 +5,12 @@ use crate::math::graph::spanning_tree::{MinimumSpanningTree, SpanningTree};
 use crate::math::graph::weighted_edge::WeightedEdge;
 use crate::math::graph::weighted_graph::WeightedGraph;
 use crate::math::number::Float;
-use std::cmp::{Ordering, Reverse};
-use std::collections::{BinaryHeap, VecDeque};
+use std::cmp::Ordering;
 
 /// Struct representing a hierarchical clustering algorithm.
+///
+/// # Type parameters
+/// * `F` - The type of distance calculation.
 #[derive(Debug, PartialEq)]
 pub struct HierarchicalClustering<F: Float> {
     hierarchy: Vec<HierarchicalNode<F>>,
@@ -66,110 +67,13 @@ where
         Self { hierarchy: nodes }
     }
 
-    #[allow(unused)]
-    #[must_use]
-    pub fn fit_with_linkage<L>(linkage: &mut L) -> Self
-    where
-        L: Linkage<F>,
-    {
-        let size = linkage.size();
-        let mut heap = BinaryHeap::new();
-        for i in 0..size - 1 {
-            for j in i + 1..size {
-                let distance = linkage.distance(i, j);
-                heap.push(Reverse(WeightedEdge::new(i, j, distance)));
-            }
-        }
-
-        let mut nodes = Vec::with_capacity(size - 1);
-        for _ in 0..size {
-            let Some(Reverse(edge)) = heap.pop() else {
-                break;
-            };
-            linkage.union(edge.u(), edge.v());
-
-            nodes.push(HierarchicalNode {
-                left: edge.u(),
-                right: edge.v(),
-                weight: edge.weight(),
-                size: 0,
-            });
-        }
-        Self { hierarchy: nodes }
-    }
-
+    /// Returns the hierarchical nodes.
     ///
     /// # Returns
     /// A reference to the hierarchical nodes.
     #[must_use]
     pub fn nodes(&self) -> &Vec<HierarchicalNode<F>> {
         &self.hierarchy
-    }
-
-    /// Partitions the dataset into `k` clusters.
-    ///
-    /// # Arguments
-    /// * `k` - The number of clusters.
-    ///
-    /// # Returns
-    /// A vector of cluster labels.
-    #[must_use]
-    pub fn partition(&self, k: usize) -> Vec<usize> {
-        let n_edge = self.hierarchy.len();
-        let n_data = n_edge + 1;
-        if k < 2 {
-            return vec![0; n_data];
-        }
-
-        let n_cluster = k.min(n_data);
-        let mut labels = vec![0; n_data];
-        let mut node_ids = BinaryHeap::new();
-        let root_node_id = n_edge * 2;
-        node_ids.push(root_node_id);
-        while node_ids.len() < n_cluster {
-            let Some(node_id) = node_ids.pop() else {
-                break;
-            };
-
-            let node = &self.hierarchy[node_id - n_data];
-            node_ids.push(node.left);
-            node_ids.push(node.right);
-        }
-
-        let mut cluster_id = 0;
-        while let Some(node_id) = node_ids.pop() {
-            if node_id >= n_data {
-                self.bfs(node_id, cluster_id, &mut labels);
-            } else {
-                labels[node_id] = cluster_id;
-            }
-            cluster_id += 1;
-        }
-        labels
-    }
-
-    fn bfs(&self, root_node_id: usize, cluster_id: usize, labels: &mut [usize]) {
-        let n_edge = self.hierarchy.len();
-        let n_data = n_edge + 1;
-        let root_node = &self.hierarchy[root_node_id - n_data];
-
-        let mut queue = VecDeque::new();
-        queue.push_back(root_node);
-        while let Some(node) = queue.pop_front() {
-            let left_id = node.left;
-            if left_id >= n_data {
-                queue.push_back(&self.hierarchy[left_id - n_data]);
-            } else {
-                labels[left_id] = cluster_id;
-            }
-
-            let right_id = node.right;
-            if right_id >= n_data {
-                queue.push_back(&self.hierarchy[right_id - n_data]);
-            } else {
-                labels[right_id] = cluster_id;
-            }
-        }
     }
 }
 
@@ -188,7 +92,6 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::math::clustering::hierarchical::linkage::SingleLinkage;
     use crate::math::distance::Distance::SquaredEuclidean;
     use crate::math::point::Point2;
 
@@ -247,67 +150,5 @@ mod tests {
                 },
             ]
         );
-    }
-
-    #[test]
-    fn test_fit_with_linkage() {
-        let dataset = sample_dataset();
-        let mut linkage = SingleLinkage::build(&dataset, |point1, point2| {
-            SquaredEuclidean.measure(point1, point2)
-        });
-        let actual = HierarchicalClustering::fit_with_linkage(&mut linkage);
-
-        assert_eq!(
-            actual.hierarchy,
-            vec![
-                HierarchicalNode {
-                    left: 4,
-                    right: 2,
-                    weight: 0.25,
-                    size: 2,
-                },
-                HierarchicalNode {
-                    left: 1,
-                    right: 3,
-                    weight: 1.0,
-                    size: 2,
-                },
-                HierarchicalNode {
-                    left: 7,
-                    right: 0,
-                    weight: 1.0,
-                    size: 3,
-                },
-                HierarchicalNode {
-                    left: 5,
-                    right: 6,
-                    weight: 1.25,
-                    size: 3,
-                },
-                HierarchicalNode {
-                    left: 9,
-                    right: 8,
-                    weight: 1.25,
-                    size: 6,
-                },
-            ]
-        );
-    }
-
-    #[test]
-    fn partition_should_partition_dataset() {
-        let dataset = sample_dataset();
-        let hierarchical_clustering = HierarchicalClustering::fit(&dataset, |u, v| {
-            let point_u = &dataset[u];
-            let point_v = &dataset[v];
-            SquaredEuclidean.measure(point_u, point_v)
-        });
-        assert_eq!(hierarchical_clustering.partition(1), vec![0, 0, 0, 0, 0, 0]);
-        assert_eq!(hierarchical_clustering.partition(2), vec![1, 1, 0, 1, 0, 0]);
-        assert_eq!(hierarchical_clustering.partition(3), vec![0, 0, 1, 0, 1, 2]);
-        assert_eq!(hierarchical_clustering.partition(4), vec![3, 0, 1, 0, 1, 2]);
-        assert_eq!(hierarchical_clustering.partition(5), vec![4, 3, 0, 2, 0, 1]);
-        assert_eq!(hierarchical_clustering.partition(6), vec![5, 4, 3, 2, 1, 0]);
-        assert_eq!(hierarchical_clustering.partition(7), vec![5, 4, 3, 2, 1, 0]);
     }
 }
--- src/math/clustering/hierarchical/mod.rs
+++ src/math/clustering/hierarchical/mod.rs
@@ -1,4 +1,3 @@
 pub mod algorithm;
-pub mod linkage;
 pub mod node;
 mod union_find;
