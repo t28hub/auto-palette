c47fd6a0f1b5bace58ec6d7d3b24cdf91d61656f
t28hub <t28oogle@gmail.com>
2023-09-10 11:19:45 +0900

    Adjust Swatch component position using correct translate classes

--- packages/auto-palette-app/src/components/Swatch.tsx
+++ packages/auto-palette-app/src/components/Swatch.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { ReactElement } from 'react';
 
 /**
@@ -12,41 +13,40 @@ interface Props {
 }
 
 /**
- * Default swatch color.
- */
-const DEFAULT_COLOR = '#FFFFFF';
-
-/**
- * Default swatch size.
- */
-const DEFAULT_SIZE = 32;
-
-/**
  * Color swatch component
  *
  * @constructor
  * @param props - Component properties
  * @return {ReactElement}
  */
-function Swatch(props: Props): ReactElement {
-  const { className, color, size, x = 0, y = 0 } = props;
-
-  const validColor = /^#[0-9a-f]{6}$/i.test(color) ? color : DEFAULT_COLOR;
-  const validSize = size && size > 0 ? size : DEFAULT_SIZE;
-
+function Swatch({ className = '', color = '#FFFFFF', size = 32, x = 0, y = 0 }: Props): ReactElement {
   const style = {
-    backgroundColor: validColor,
-    width: validSize,
-    height: validSize,
+    backgroundColor: color,
+    width: size,
+    height: size,
     left: x,
     top: y,
   };
 
   return (
     <div
-      className={`flex justify-center items-center absolute z-10 translate-x-1/2 translate-y-1/2 cursor-pointer drop-shadow-md rounded-full border-white border-solid border-4 border-opacity-90 ${
-        className || ''
-      }`}
+      className={clsx(
+        'flex',
+        'justify-center',
+        'items-center',
+        'absolute',
+        'z-10',
+        '-translate-x-1/2',
+        '-translate-y-1/2',
+        'cursor-pointer',
+        'drop-shadow-md',
+        'rounded-full',
+        'border-white',
+        'border-solid',
+        'border-4',
+        'border-opacity-90',
+        className,
+      )}
       style={style}
     ></div>
   );
