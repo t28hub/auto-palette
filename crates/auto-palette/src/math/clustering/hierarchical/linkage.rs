41719b96cfaf2badf645e93b71d691a0e6297ec0
t28hub <t28oogle@gmail.com>
2023-11-05 15:23:55 +0900

    Implement Complement linkage for HierarchicalClustering

--- crates/auto-palette/src/math/clustering/hierarchical/linkage.rs
+++ crates/auto-palette/src/math/clustering/hierarchical/linkage.rs
@@ -2,13 +2,33 @@ use crate::math::distance::DistanceMetric;
 use crate::math::point::Point;
 use crate::number::Float;
 
+/// Trait representing a linkage.
+///
+/// # Type Parameters
+/// * `F` - The float type used for calculations (e.g., f32 or f64).
 pub trait Linkage<F>
 where
     F: Float,
 {
+    /// Returns the distance between the points with the given indices.
+    ///
+    /// # Arguments
+    /// * `i` - The index of the 1st point.
+    /// * `j` - The index of the 2nd point.
+    ///
+    /// # Returns
+    /// The distance between the points with the given indices.
     #[must_use]
     fn distance(&self, i: usize, j: usize) -> F;
 
+    /// Merges the points with the given indices.
+    ///
+    /// # Arguments
+    /// * `i` - The index of the 1st point.
+    /// * `j` - The index of the 2nd point.
+    ///
+    /// # Returns
+    /// The label of the new merged point.
     #[must_use]
     fn merge(&mut self, i: usize, j: usize) -> usize;
 }
@@ -154,5 +174,133 @@ where
         }
         self.next_index += 1;
         label
+    }
+}
+
+/// Struct representing a complete linkage.
+///
+/// # Type Parameters
+/// * `F` - The float type used for calculations (e.g., f32 or f64).
+#[derive(Debug)]
+pub struct CompleteLinkage<F>
+where
+    F: Float,
+{
+    matrix: DistanceMatrix<F>,
+    next_index: usize,
+}
+
+impl<F> CompleteLinkage<F>
+where
+    F: Float,
+{
+    /// Creates a new `CompleteLinkage` instance.
+    ///
+    /// # Type Parameters
+    /// * `P` - The type of points.
+    ///
+    /// # Arguments
+    /// * `points` - The points to use for calculating distances.
+    /// * `metric` - The distance metric to use.
+    ///
+    /// # Returns
+    /// A new `CompleteLinkage` instance.
+    #[must_use]
+    pub fn new<'a, P>(points: &'a [P], metric: &'a DistanceMetric) -> Self
+    where
+        P: Point<F>,
+    {
+        Self {
+            matrix: DistanceMatrix::new(points, metric),
+            next_index: points.len(),
+        }
+    }
+}
+
+impl<F> Linkage<F> for CompleteLinkage<F>
+where
+    F: Float,
+{
+    #[must_use]
+    fn distance(&self, i: usize, j: usize) -> F {
+        self.matrix.get(i, j)
+    }
+
+    #[must_use]
+    fn merge(&mut self, i: usize, j: usize) -> usize {
+        assert!(i < j, "i must be less than j: {} < {}", i, j);
+
+        let label = self.next_index;
+        for k in 0..label {
+            let distance1 = self.distance(i, k);
+            let distance2 = self.distance(j, k);
+            if i == k {
+                self.matrix.set(k, label, distance2);
+            } else if j == k {
+                self.matrix.set(k, label, distance1);
+            } else {
+                self.matrix.set(k, label, distance1.max(distance2));
+            }
+        }
+        self.next_index += 1;
+        label
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::math::point::Point2;
+
+    #[test]
+    fn test_single_linkage() {
+        let points = vec![
+            Point2(0.0, 0.0),
+            Point2(1.0, 0.0),
+            Point2(0.0, 1.0),
+            Point2(1.0, 1.0),
+        ];
+        let mut linkage = SingleLinkage::new(&points, &DistanceMetric::SquaredEuclidean);
+        assert_eq!(linkage.distance(0, 1), 1.0);
+        assert_eq!(linkage.distance(0, 2), 1.0);
+        assert_eq!(linkage.distance(0, 3), 2.0);
+        assert_eq!(linkage.distance(1, 2), 2.0);
+        assert_eq!(linkage.distance(1, 3), 1.0);
+        assert_eq!(linkage.distance(2, 3), 1.0);
+        assert_eq!(linkage.distance(0, 4), f64::MAX);
+        assert_eq!(linkage.distance(0, 5), f64::MAX);
+
+        assert_eq!(linkage.merge(0, 1), 4);
+        assert_eq!(linkage.distance(0, 4), 1.0);
+        assert_eq!(linkage.distance(1, 4), 1.0);
+        assert_eq!(linkage.distance(2, 4), 1.0);
+        assert_eq!(linkage.distance(3, 4), 1.0);
+        assert_eq!(linkage.distance(4, 5), f64::MAX);
+    }
+
+    #[test]
+    fn test_complete_linkage() {
+        let points = vec![
+            Point2(0.0, 0.0),
+            Point2(1.0, 0.0),
+            Point2(0.0, 1.0),
+            Point2(1.0, 1.0),
+        ];
+        let mut linkage = CompleteLinkage::new(&points, &DistanceMetric::SquaredEuclidean);
+        assert_eq!(linkage.distance(0, 1), 1.0);
+        assert_eq!(linkage.distance(0, 2), 1.0);
+        assert_eq!(linkage.distance(0, 3), 2.0);
+        assert_eq!(linkage.distance(1, 2), 2.0);
+        assert_eq!(linkage.distance(1, 3), 1.0);
+        assert_eq!(linkage.distance(2, 3), 1.0);
+        assert_eq!(linkage.distance(0, 4), f64::MAX);
+        assert_eq!(linkage.distance(0, 5), f64::MAX);
+
+        assert_eq!(linkage.merge(0, 1), 4);
+        assert_eq!(linkage.distance(0, 4), 1.0);
+        assert_eq!(linkage.distance(1, 4), 1.0);
+        assert_eq!(linkage.distance(2, 4), 2.0);
+        assert_eq!(linkage.distance(3, 4), 2.0);
+        assert_eq!(linkage.distance(4, 5), f64::MAX);
     }
 }
