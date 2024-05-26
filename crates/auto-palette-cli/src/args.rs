409b3651463468432562d9d36d1e3465f2eb41d6
Tatsuya Maki <t28oogle@gmail.com>
2024-05-26 13:10:26 +0900

    Improve CLI option descriptions for better user comprehension in auto-palette (#54)

--- crates/auto-palette-cli/src/args.rs
+++ crates/auto-palette-cli/src/args.rs
@@ -17,7 +17,7 @@ pub struct Options {
     #[arg(
         value_name = "IMAGE",
         help = "Path to the image file.",
-        long_help = "Path to the image file. Supported formats: PNG, JPEG, GIF, BMP, ICO, and TIFF.",
+        long_help = "Path to the image file. Supported formats include PNG, JPEG, GIF, BMP, ICO, and TIFF.",
         required = true,
         value_hint = ValueHint::FilePath,
     )]
@@ -28,27 +28,27 @@ pub struct Options {
         short = 'a',
         value_name = "name",
         value_enum,
-        help = "Algorithm for extracting the palette.",
-        default_value_t = AlgorithmArg::default(),
+        help = "Algorithm for extracting the color palette.",
+        default_value_t = AlgorithmOption::default(),
         ignore_case = true,
     )]
-    pub algorithm: AlgorithmArg,
+    pub algorithm: AlgorithmOption,
 
     #[arg(
         long,
         short = 't',
         value_name = "name",
         value_enum,
-        help = "Theme for selecting the swatches.",
+        help = "Theme for selecting the swatches from the extracted color palette",
         ignore_case = true
     )]
-    pub theme: Option<ThemeArg>,
+    pub theme: Option<ThemeOption>,
 
     #[arg(
         long,
         short = 'n',
         value_name = "number",
-        help = "Number of swatches to extract.",
+        help = "Number of prominent colors to select from the extracted color palette.",
         default_value = "5"
     )]
     pub count: usize,
@@ -58,79 +58,110 @@ pub struct Options {
         short = 'c',
         value_name = "name",
         value_enum,
-        help = "Output color space for the swatches.",
-        default_value_t = ColorArg::default(),
+        help = "Output color space for the extracted colors",
+        default_value_t = ColorOption::default(),
         ignore_case = true,
     )]
-    pub color: ColorArg,
+    pub color: ColorOption,
 
-    #[arg(long, help = "Disable image resizing before extracting the palette.")]
+    #[arg(
+        long,
+        help = "Disable image resizing before extracting the color palette.",
+        long_help = "Disable image resizing before extracting the color palette. This potentially improve the accuracy of the results by preserving the original image resolution."
+    )]
     pub no_resize: bool,
 }
 
+/// The algorithm options for extracting the color palette from the image.
 #[derive(Debug, Default, Copy, Clone, ValueEnum)]
-pub enum AlgorithmArg {
+pub enum AlgorithmOption {
     #[default]
-    #[clap(name = "dbscan")]
+    #[clap(
+        name = "dbscan",
+        help = "High accuracy but slower speed. Ideal for precision over performance."
+    )]
     Dbscan,
-    #[clap(name = "dbscan++")]
+    #[clap(
+        name = "dbscan++",
+        help = "A balanced algorithm with faster speed and good accuracy. Ideal for a balance between precision and performance."
+    )]
     DbscanPlusPlus,
-    #[clap(name = "kmeans")]
+    #[clap(
+        name = "kmeans",
+        help = "Fast speed but potentially less accurate. Ideal for performance over precision."
+    )]
     KMeans,
 }
 
-impl From<AlgorithmArg> for Algorithm {
-    fn from(arg: AlgorithmArg) -> Self {
-        match arg {
-            AlgorithmArg::Dbscan => Algorithm::DBSCAN,
-            AlgorithmArg::DbscanPlusPlus => Algorithm::DBSCANpp,
-            AlgorithmArg::KMeans => Algorithm::KMeans,
+impl From<AlgorithmOption> for Algorithm {
+    fn from(option: AlgorithmOption) -> Self {
+        match option {
+            AlgorithmOption::Dbscan => Algorithm::DBSCAN,
+            AlgorithmOption::DbscanPlusPlus => Algorithm::DBSCANpp,
+            AlgorithmOption::KMeans => Algorithm::KMeans,
         }
     }
 }
 
-/// The theme options supported by the `auto-palette` command.
+/// The theme options for selecting the swatches from the extracted color palette.
 #[derive(Debug, Copy, Clone, ValueEnum)]
-pub enum ThemeArg {
+pub enum ThemeOption {
+    #[clap(
+        name = "basic",
+        help = "A general-purpose theme suitable for a wide range of images."
+    )]
     Basic,
+    #[clap(name = "vivid", help = "Prioritize saturated colors.")]
     Vivid,
+    #[clap(name = "muted", help = "Prioritize desaturated colors.")]
     Muted,
+    #[clap(name = "light", help = "Prioritize light colors.")]
     Light,
+    #[clap(name = "dark", help = "Prioritize dark colors.")]
     Dark,
 }
 
-impl From<ThemeArg> for Theme {
-    fn from(arg: ThemeArg) -> Self {
-        match arg {
-            ThemeArg::Basic => Theme::Basic,
-            ThemeArg::Vivid => Theme::Vivid,
-            ThemeArg::Muted => Theme::Muted,
-            ThemeArg::Light => Theme::Light,
-            ThemeArg::Dark => Theme::Dark,
+impl From<ThemeOption> for Theme {
+    fn from(option: ThemeOption) -> Self {
+        match option {
+            ThemeOption::Basic => Theme::Basic,
+            ThemeOption::Vivid => Theme::Vivid,
+            ThemeOption::Muted => Theme::Muted,
+            ThemeOption::Light => Theme::Light,
+            ThemeOption::Dark => Theme::Dark,
         }
     }
 }
 
-/// The color options supported by the `auto-palette` command.
+/// The color space options for the extracted colors.
 #[derive(Debug, Default, Copy, Clone, ValueEnum)]
-pub enum ColorArg {
+pub enum ColorOption {
     #[default]
+    #[clap(name = "hex", help = "Hexadecimal color representation")]
     Hex,
+    #[clap(name = "rgb", help = "RGB color space")]
     Rgb,
+    #[clap(name = "hsl", help = "HSL color space")]
     Hsl,
+    #[clap(name = "hsv", help = "HSV color space")]
     Hsv,
+    #[clap(name = "lab", help = "CIE L*a*b* color space")]
     Lab,
+    #[clap(name = "luv", help = "CIE L*u*v* color space")]
     Luv,
-    #[clap(name = "lchab")]
+    #[clap(name = "lchab", help = "LCH(ab) color space")]
     LCHab,
-    #[clap(name = "lchuv")]
+    #[clap(name = "lchuv", help = "LCH(uv) color space")]
     LCHuv,
+    #[clap(name = "oklab", help = "Oklab color space")]
     Oklab,
+    #[clap(name = "oklch", help = "Oklch color space")]
     Oklch,
+    #[clap(name = "xyz", help = "CIE XYZ color space")]
     Xyz,
 }
 
-impl ColorArg {
+impl ColorOption {
     /// Returns the string representation of the color space for the given color.
     ///
     /// # Arguments
