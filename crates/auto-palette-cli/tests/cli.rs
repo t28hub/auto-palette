96d8c31fe7e758f824a09e3f847cac7f8a6ea714
Tatsuya Maki <t28oogle@gmail.com>
2024-08-13 18:29:58 +0900

    Add tests for CLI workspace and update GitHub Actions to run the tests

--- crates/auto-palette-cli/tests/cli.rs
+++ crates/auto-palette-cli/tests/cli.rs
@@ -18,6 +18,8 @@ fn test_cli() {
         .arg("dbscan")
         .arg("--count")
         .arg("6")
+        .arg("--output")
+        .arg("table")
         .arg("--no-resize")
         .assert()
         .stdout(
@@ -29,6 +31,97 @@ fn test_cli() {
                 .and(predicate::str::contains("Extracted 6 swatch(es) in")),
         );
     assert.success();
+}
+
+#[test]
+fn test_missing_path() {
+    let assert = auto_palette().assert().stderr(predicate::str::contains(
+        "🎨 A CLI tool to extract prominent color palettes from images.",
+    ));
+    assert.failure();
+}
+
+#[test]
+fn test_invalid_path() {
+    let assert = auto_palette()
+        .arg("invalid.png")
+        .assert()
+        .stderr(predicate::str::contains(
+            "Failed to open the image file \"invalid.png\"",
+        ));
+    assert.failure();
+}
+
+#[test]
+fn test_algorithm() {
+    let assert = auto_palette()
+        .arg("../../gfx/olympic_logo.png")
+        .arg("--algorithm")
+        .arg("kmeans")
+        .assert();
+    assert.success();
+}
+
+#[test]
+fn test_invalid_algorithm() {
+    let assert = auto_palette()
+        .arg("../../gfx/olympic_logo.png")
+        .arg("--algorithm")
+        .arg("unknown")
+        .assert()
+        .stderr(predicate::str::contains(
+            "invalid value 'unknown' for '--algorithm <name>'",
+        ));
+    assert.failure();
+}
+
+#[test]
+fn test_theme() {
+    let assert = auto_palette()
+        .arg("../../gfx/olympic_logo.png")
+        .arg("--theme")
+        .arg("light")
+        .assert();
+    assert.success();
+}
+
+#[test]
+fn test_invalid_theme() {
+    let assert = auto_palette()
+        .arg("../../gfx/olympic_logo.png")
+        .arg("--theme")
+        .arg("unknown")
+        .assert()
+        .stderr(predicate::str::contains(
+            "invalid value 'unknown' for '--theme <name>'",
+        ));
+    assert.failure();
+}
+
+#[test]
+fn test_invalid_count() {
+    let assert = auto_palette()
+        .arg("../../gfx/olympic_logo.png")
+        .arg("--count")
+        .arg("0")
+        .assert()
+        .stderr(predicate::str::contains(
+            "invalid value '0' for '--count <count>': must be a positive integer",
+        ));
+    assert.failure();
+}
+
+#[test]
+fn test_invalid_output() {
+    let assert = auto_palette()
+        .arg("../../gfx/olympic_logo.png")
+        .arg("--output")
+        .arg("unknown")
+        .assert()
+        .stderr(predicate::str::contains(
+            "invalid value 'unknown' for '--output <name>'",
+        ));
+    assert.failure();
 }
 
 #[test]
