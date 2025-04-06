ae04067d74d4924b2b981d32320d3d0e598d881c
Tatsuya Maki <t28oogle@gmail.com>
2025-04-06 15:44:29 +0900

    Configure build settings for `auto-palette-wasm` package to publish NPM distribution (#154)

---
+++ packages/auto-palette-wasm/tsup.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'tsup';
+import { copy } from 'esbuild-plugin-copy';
+
+export default defineConfig({
+  target: 'esnext',
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  sourcemap: false,
+  shims: true,
+  minify: true,
+  clean: false,
+  esbuildPlugins: [
+    copy({
+      resolveFrom: 'cwd',
+      assets: [
+        {
+          from: '../../crates/auto-palette-wasm/dist/*.wasm',
+          to: './dist',
+        },
+      ],
+    }),
+  ],
+});
