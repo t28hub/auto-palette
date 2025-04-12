8b6ee545c50eefb510f480e59bc4f3ce9522949d
Tatsuya Maki <t28oogle@gmail.com>
2025-04-12 15:22:42 +0900

    Add benchmark tests to compare the performance of @auto-palette/wasm and auto-palette-ts

---
+++ packages/auto-palette-wasm/test/palette.bench.ts
@@ -0,0 +1,102 @@
+import * as AutoPaletteWasm from '@auto-palette/wasm';
+import * as AutoPaletteTs from 'auto-palette';
+import { bench, describe, expect } from 'vitest';
+import { resolve } from 'node:path';
+import { loadImageData } from './image';
+
+const IMAGE_PATH = resolve(
+  process.cwd(),
+  '../../gfx/laura-clugston-pwW2iV9TZao-unsplash.jpg',
+);
+
+describe('benchmark @auto-palette/wasm vs auto-palette-ts', () => {
+  bench('extract with DBSCAN algorithm in WebAssembly', async () => {
+    // Arrange
+    const imageData = await loadImageData(IMAGE_PATH);
+
+    // Act
+    const palette = AutoPaletteWasm.Palette.extract(imageData, 'dbscan');
+    const swatches = palette.findSwatches(6);
+
+    // Assert
+    expect(palette.isEmpty()).toBeFalsy();
+    expect(palette.length).toBeGreaterThan(32);
+    expect(swatches).toHaveLength(6);
+  });
+
+  bench('extract with DBSCAN++ algorithm in WebAssembly', async () => {
+    // Arrange
+    const imageData = await loadImageData(IMAGE_PATH);
+
+    // Act
+    const palette = AutoPaletteWasm.Palette.extract(imageData, 'dbscan++');
+    const swatches = palette.findSwatches(6);
+
+    // Assert
+    expect(palette.isEmpty()).toBeFalsy();
+    expect(palette.length).toBeGreaterThan(32);
+    expect(swatches).toHaveLength(6);
+  });
+
+  bench('extract with Kmeans algorithm in WebAssembly', async () => {
+    // Arrange
+    const imageData = await loadImageData(IMAGE_PATH);
+
+    // Act
+    const palette = AutoPaletteWasm.Palette.extract(imageData, 'kmeans');
+    const swatches = palette.findSwatches(6);
+
+    // Assert
+    expect(palette.isEmpty()).toBeFalsy();
+    expect(palette.length).toBeGreaterThan(32);
+    expect(swatches).toHaveLength(6);
+  });
+
+  bench('extract with DBSCAN algorithm in TypeScript', async () => {
+    // Arrange
+    const imageData = await loadImageData(IMAGE_PATH);
+
+    // Act
+    const palette = AutoPaletteTs.Palette.extract(imageData, {
+      algorithm: 'dbscan',
+    });
+    const swatches = palette.findSwatches(6);
+
+    // Assert
+    expect(palette.isEmpty()).toBeFalsy();
+    expect(palette.size()).toBeGreaterThan(32);
+    expect(swatches).toHaveLength(6);
+  });
+
+  bench('extract with DBSCAN++ algorithm in TypeScript', async () => {
+    // Arrange
+    const imageData = await loadImageData(IMAGE_PATH);
+
+    // Act
+    const palette = AutoPaletteTs.Palette.extract(imageData, {
+      algorithm: 'dbscan++',
+    });
+    const swatches = palette.findSwatches(6);
+
+    // Assert
+    expect(palette.isEmpty()).toBeFalsy();
+    expect(palette.size()).toBeGreaterThan(32);
+    expect(swatches).toHaveLength(6);
+  });
+
+  bench('extract with Kmeans algorithm in TypeScript', async () => {
+    // Arrange
+    const imageData = await loadImageData(IMAGE_PATH);
+
+    // Act
+    const palette = AutoPaletteTs.Palette.extract(imageData, {
+      algorithm: 'kmeans',
+    });
+    const swatches = palette.findSwatches(6);
+
+    // Assert
+    expect(palette.isEmpty()).toBeFalsy();
+    expect(palette.size()).toBeGreaterThan(32);
+    expect(swatches).toHaveLength(6);
+  });
+});
